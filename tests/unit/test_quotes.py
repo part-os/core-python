e6e4d561d67139f88cfc5d14bc68c2b184d89ee7
Rob Carrington <rob.carrington@paperlessparts.com>
2020-01-02 13:50:45 -0500

    Flesh out unit test for quotes

--- tests/unit/test_quotes.py
+++ tests/unit/test_quotes.py
@@ -1,5 +1,6 @@
 import unittest
 import json
+from decimal import Decimal
 from unittest.mock import MagicMock
 
 from paperless.objects.quotes import Quote
@@ -22,3 +23,34 @@ class TestQuotes(unittest.TestCase):
         # test customer
         customer = q.customer
         self.assertEqual(customer.last_name, 'Customer')
+        # test company
+        company = customer.company
+        self.assertEqual(company.business_name, 'Outside Firm')
+        # test metrics
+        metrics = company.metrics
+        self.assertEqual(metrics.order_revenue_all_time.dollars, 0)
+        # test quote items
+        quote_item = q.quote_items[0]
+        self.assertEqual(quote_item.id, 24988)
+        self.assertEqual(quote_item.type, 'automatic')
+        self.assertEqual(quote_item.component_ids[0], 28607)
+        # test root component
+        root_component = quote_item.root_component
+        self.assertEqual(root_component.id, 28600)
+        self.assertEqual(root_component.type, 'assembled')
+        self.assertEqual(root_component.part.filename, 'small-sub-assembly.STEP')
+        # test quantities
+        quantity = root_component.quantities[0]
+        self.assertEqual(quantity.quantity, 1)
+        self.assertEqual(quantity.unit_price.dollars, Decimal('3086.88'))
+        # test operations
+        operation = root_component.operations[0]
+        self.assertEqual(operation.name, 'Chromate')
+        # test process
+        process = root_component.process
+        self.assertEqual(process.name, 'CNC Machining')
+        # test material
+        material = root_component.material
+        self.assertEqual(material.name, 'Aluminium 6061')
+
+
