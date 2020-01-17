7882265069150d7b62f4f0b8906ec3aefb18ed2e
Rob Carrington <rob.carrington@paperlessparts.com>
2020-01-17 15:34:13 -0500

    Add add ons to orders

--- tests/unit/test_orders.py
+++ tests/unit/test_orders.py
@@ -1,5 +1,6 @@
 import unittest
 import json
+from decimal import Decimal
 from unittest.mock import MagicMock
 
 from paperless.objects.orders import Order
@@ -16,21 +17,22 @@ class TestOrders(unittest.TestCase):
     def test_get_order(self):
         self.client.get_resource = MagicMock(return_value=self.mock_order_json)
         o = Order.get(1)
-        self.assertEqual(o.number, 14)
+        self.assertEqual(o.number, 16)
         self.assertEqual('credit_card', o.payment_details.payment_type)
         self.assertEqual('pending', o.status)
-        self.assertEqual(67, o.quote_number)
+        self.assertEqual(73, o.quote_number)
         self.assertEqual(len(o.order_items), 3)
         # test assembly order item
         assmb_oi = o.order_items[0]
-        self.assertEqual(assmb_oi.id, 15845)
+        self.assertEqual(assmb_oi.id, 17008)
         self.assertEqual(len(assmb_oi.components), 8)
-        self.assertEqual((assmb_oi.quote_item_id), 24988)
+        self.assertEqual((assmb_oi.quote_item_id), 27010)
         # test single component order item
         standard_oi = o.order_items[1]
-        self.assertEqual(standard_oi.id, 15846)
-        self.assertEqual(standard_oi.root_component_id, 28608)
+        self.assertEqual(standard_oi.id, 17009)
+        self.assertEqual(standard_oi.root_component_id, 30747)
         self.assertEqual(len(standard_oi.components), 1)
+        self.assertIsNone(standard_oi.add_on_fees)
         root_component = standard_oi.components[0]
         self.assertEqual(len(root_component.material_operations), 2)
         self.assertEqual(len(root_component.shop_operations), 7)
@@ -39,6 +41,11 @@ class TestOrders(unittest.TestCase):
         self.assertEqual(finish_op.cost.dollars, 150.)
         self.assertIsNone(finish_op.setup_time)
         self.assertIsNone(finish_op.get_variable('bad name'))
+        # test add ons
+        other_oi = o.order_items[0]
+        self.assertEqual(other_oi.base_price.dollars, Decimal('3086.88'))
+        add_on = other_oi.ordered_add_ons[0]
+        self.assertEqual(add_on.quantity, 1)
         # test manual line item
         manual_oi = o.order_items[2]
         self.assertEqual('manual', manual_oi.quote_item_type)
@@ -49,11 +56,11 @@ class TestOrders(unittest.TestCase):
         o = Order.get(1)
         oi = o.order_items[0]
         self.assertEqual(2020, oi.ships_on_dt.year)
-        self.assertEqual(1, oi.ships_on_dt.month)
-        self.assertEqual(9, oi.ships_on_dt.day)
-        self.assertEqual(2019, o.created_dt.year)
-        self.assertEqual(12, o.created_dt.month)
-        self.assertEqual(18, o.created_dt.day)
+        self.assertEqual(2, oi.ships_on_dt.month)
+        self.assertEqual(5, oi.ships_on_dt.day)
+        self.assertEqual(2020, o.created_dt.year)
+        self.assertEqual(1, o.created_dt.month)
+        self.assertEqual(17, o.created_dt.day)
 
     def test_ship_desc(self):
         from paperless.objects.orders import ShippingOption
