ea6ba12e653a4d845ff50fec4b13611d3bd7984c
Rob Carrington <rob.carrington@paperlessparts.com>
2020-06-23 14:38:32 -0400

    Set up the objects and mappers for the company details endpoint

--- paperless/api_mappers/customers.py
+++ paperless/api_mappers/customers.py
@@ -11,6 +11,46 @@ class PaymentTermsDetailsMapper(BaseMapper):
         return mapped_result
 
 
+class CountryMapper(BaseMapper):
+    @classmethod
+    def map(cls, resource):
+        mapped_result = {}
+        field_keys = ['abbr', 'name']
+        for key in field_keys:
+            mapped_result[key] = resource.get(key, None)
+        return mapped_result
+
+
+class StateMapper(BaseMapper):
+    @classmethod
+    def map(cls, resource):
+        mapped_result = {}
+        field_keys = ['abbr', 'name']
+        for key in field_keys:
+            mapped_result[key] = resource.get(key, None)
+        return mapped_result
+
+
+class AddressMapper(BaseMapper):
+    @classmethod
+    def map(cls, resource):
+        mapped_result = {}
+        mapped_result['country'] = CountryMapper.map(resource['country'])
+        mapped_result['state'] = CountryMapper.map(resource['state'])
+        return mapped_result
+
+
+class AddressInfoMapper(BaseMapper):
+    @classmethod
+    def map(cls, resource):
+        mapped_result = {}
+        field_keys = ['business_name', 'first_name', 'last_name', 'phone', 'phone_ext']
+        for key in field_keys:
+            mapped_result[key] = resource.get(key, None)
+        mapped_result['address'] = AddressMapper.map(resource['address'])
+        return mapped_result
+
+
 class CompanyListMapper(BaseMapper):
     @classmethod
     def map(cls, resource):
@@ -18,4 +58,21 @@ class CompanyListMapper(BaseMapper):
         field_keys = ['business_name', 'erp_code', 'id', 'phone', 'phone_ext', 'slug']
         for key in field_keys:
             mapped_result[key] = resource.get(key, None)
+        return mapped_result
+
+
+class CompanyMapper(BaseMapper):
+    @classmethod
+    def map(cls, resource):
+        mapped_result = {}
+        field_keys = ['business_name', 'created', 'credit_line', 'erp_code', 'id', 'notes',
+                      'phone', 'phone_ext', 'slug', 'tax_rate', 'url']
+        for key in field_keys:
+            mapped_result[key] = resource.get(key, None)
+        boolean_keys = ['purchase_orders_enabled', 'tax_exempt']
+        for key in boolean_keys:
+            mapped_result[key] = resource.get(key, False)
+        mapped_result['billing_info'] = AddressInfoMapper.map(resource['billing_info']) if resource['billing_info'] else None
+        mapped_result['payment_terms'] = PaymentTermsDetailsMapper.map(resource['payment_terms']) if resource['payment_terms'] else None
+        mapped_result['shipping_info'] = AddressInfoMapper.map(resource['shipping_info']) if resource['shipping_info'] else None
         return mapped_result
--- paperless/objects/customers.py
+++ paperless/objects/customers.py
@@ -3,7 +3,7 @@ from typing import Optional, List
 
 import attr
 
-from paperless.api_mappers.customers import PaymentTermsDetailsMapper, CompanyListMapper
+from paperless.api_mappers.customers import PaymentTermsDetailsMapper, CompanyListMapper, CompanyMapper
 from paperless.client import PaperlessClient
 from paperless.mixins import FromJSONMixin, ListMixin, ReadMixin, ToDictMixin, CreateMixin, PaginatedListMixin
 from .common import Money
@@ -26,6 +26,34 @@ class PaymentTerms(FromJSONMixin, ListMixin):
 
 
 @attr.s(frozen=True)
+class Country:
+    abbr: str = attr.ib(validator=attr.validators.instance_of(str))
+    name: str = attr.ib(validator=attr.validators.instance_of(str))
+
+
+@attr.s(frozen=True)
+class State:
+    abbr: str = attr.ib(validator=attr.validators.instance_of(str))
+    name: str = attr.ib(validator=attr.validators.instance_of(str))
+
+
+@attr.s(frozen=True)
+class Address:
+    country: Country = attr.ib(converter=convert_cls(Country))
+    state: State = attr.ib(converter=convert_cls(State))
+
+
+@attr.s(frozen=True)
+class AddressInfo:
+    address: Address = attr.ib(converter=convert_cls(Address))
+    business_name: str = attr.ib(validator=attr.validators.instance_of(str))
+    first_name: str = attr.ib(validator=attr.validators.instance_of(str))
+    last_name: str = attr.ib(validator=attr.validators.instance_of(str))
+    phone: str = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(str)))
+    phone_ext: str = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(str)))
+
+
+@attr.s(frozen=True)
 class CompanyList(FromJSONMixin, PaginatedListMixin):
 
     _mapper = CompanyListMapper
@@ -39,4 +67,30 @@ class CompanyList(FromJSONMixin, PaginatedListMixin):
 
     @classmethod
     def construct_list_url(cls):
-        return 'companies/public/new'
+        return 'companies/public/new'  # TODO - remove the /new
+
+@attr.s(frozen=True)
+class Company(FromJSONMixin, ReadMixin):
+
+    _mapper = CompanyMapper
+
+    billing_info: AddressInfo = attr.ib(converter=convert_cls(AddressInfo))
+    business_name: str = attr.ib(validator=attr.validators.instance_of(str))
+    created: str = attr.ib(validator=attr.validators.instance_of(str))  # TODO - should we convert to datetime?
+    credit_line: Optional[Money] = attr.ib(converter=optional_convert(Money), validator=attr.validators.optional(attr.validators.instance_of(Money)))
+    erp_code: str = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(str)))
+    id: int = attr.ib(validator=attr.validators.instance_of(int))
+    notes: str = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(str)))
+    payment_terms: PaymentTerms = attr.ib(converter=convert_cls(PaymentTerms))
+    phone: str = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(str)))
+    phone_ext: str = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(str)))
+    purchase_orders_enabled: bool = attr.ib(validator=attr.validators.instance_of(bool))
+    shipping_info: AddressInfo = attr.ib(converter=convert_cls(AddressInfo))
+    slug: str = attr.ib(validator=attr.validators.instance_of(str))
+    tax_exempt: bool = attr.ib(validator=attr.validators.instance_of(bool))
+    tax_rate: float = attr.ib(validator=numeric_validator)
+    url: str = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(str)))
+
+    @classmethod
+    def construct_get_url(cls):
+        return 'companies/public'
