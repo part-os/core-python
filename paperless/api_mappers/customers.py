0666360c1b73560cde79fe0d0fb86848d2c9240f
John <john.peck@paperlessparts.com>
2021-01-25 14:36:24 -0500

    added black

--- paperless/api_mappers/customers.py
+++ paperless/api_mappers/customers.py
@@ -1,18 +1,42 @@
-from paperless.api_mappers import BaseMapper
+from paperless.api_mappers import (
+    BaseMapper,
+)
 
 
 class AccountMapper(BaseMapper):
     @classmethod
     def map(cls, resource):
         mapped_result = {}
-        field_keys = ['name', 'created', 'credit_line', 'erp_code', 'id', 'notes', 'payment_terms',
-                      'payment_terms_period', 'phone', 'phone_ext', 'purchase_orders_enabled',
-                      'sold_to_address', 'tax_exempt', 'tax_rate', 'url']
+        field_keys = [
+            'name',
+            'created',
+            'credit_line',
+            'erp_code',
+            'id',
+            'notes',
+            'payment_terms',
+            'payment_terms_period',
+            'phone',
+            'phone_ext',
+            'purchase_orders_enabled',
+            'sold_to_address',
+            'tax_exempt',
+            'tax_rate',
+            'url',
+        ]
+        boolean_keys = ['purchase_orders_enabled', 'tax_exempt']
         for key in field_keys:
             mapped_result[key] = resource.get(key, None)
-        boolean_keys = ['purchase_orders_enabled', 'tax_exempt']
-        mapped_result['sold_to_address'] = AddressMapper.map(resource['sold_to_address']) if resource['sold_to_address'] else None
-        mapped_result['billing_addresses'] = map(BillingAddressMapper.map, resource['billing_addresses'])
+        for key in boolean_keys:
+            mapped_result[key] = resource.get(key, False)
+        mapped_result['sold_to_address'] = (
+            AddressMapper.map(resource['sold_to_address'])
+            if resource['sold_to_address']
+            else None
+        )
+        mapped_result['billing_addresses'] = map(
+            BillingAddressMapper.map, resource['billing_addresses']
+        )
         return mapped_result
 
 
@@ -30,8 +54,19 @@ class AddressInfoMapper(BaseMapper):
     @classmethod
     def map(cls, resource):
         mapped_result = {}
-        field_keys = ['address1', 'address2', 'business_name', 'city', 'country', 'first_name',
-                      'last_name', 'phone', 'phone_ext', 'postal_code', 'state']
+        field_keys = [
+            'address1',
+            'address2',
+            'business_name',
+            'city',
+            'country',
+            'first_name',
+            'last_name',
+            'phone',
+            'phone_ext',
+            'postal_code',
+            'state',
+        ]
         for key in field_keys:
             mapped_result[key] = resource.get(key, None)
         return mapped_result
@@ -51,7 +86,15 @@ class BillingAddressMapper(BaseMapper):
     @classmethod
     def map(cls, resource):
         mapped_result = {}
-        field_keys = ['address1', 'address2', 'city', 'country', 'id', 'postal_code', 'state']
+        field_keys = [
+            'address1',
+            'address2',
+            'city',
+            'country',
+            'id',
+            'postal_code',
+            'state',
+        ]
         for key in field_keys:
             mapped_result[key] = resource.get(key, None)
         return mapped_result
@@ -62,8 +105,14 @@ class ContactListMapper(BaseMapper):
     def map(cls, resource):
         mapped_result = {}
         field_keys = [
-            'account_id', 'created', 'email', 'first_name', 'id', 'last_name', 'phone',
-            'phone_ext'
+            'account_id',
+            'created',
+            'email',
+            'first_name',
+            'id',
+            'last_name',
+            'phone',
+            'phone_ext',
         ]
         for key in field_keys:
             mapped_result[key] = resource.get(key, None)
@@ -74,10 +123,22 @@ class ContactMapper(BaseMapper):
     @classmethod
     def map(cls, resource):
         mapped_result = {}
-        field_keys = ['account_id', 'created','email', 'first_name', 'id', 'last_name', 'notes', 'phone', 'phone_ext']
+        field_keys = [
+            'account_id',
+            'created',
+            'email',
+            'first_name',
+            'id',
+            'last_name',
+            'notes',
+            'phone',
+            'phone_ext',
+        ]
         for key in field_keys:
             mapped_result[key] = resource.get(key, None)
-        mapped_result['address'] = AddressMapper.map(resource['address']) if resource['address'] else None
+        mapped_result['address'] = (
+            AddressMapper.map(resource['address']) if resource['address'] else None
+        )
         return mapped_result
 
 
@@ -85,10 +146,12 @@ class FacilityMapper(BaseMapper):
     @classmethod
     def map(cls, resource):
         mapped_result = {}
-        field_keys =['account_id', 'attention', 'created', 'id', 'name']
+        field_keys = ['account_id', 'attention', 'created', 'id', 'name']
         for key in field_keys:
             mapped_result[key] = resource.get(key, None)
-        mapped_result['address'] = AddressMapper.map(resource['address']) if resource['address'] else None
+        mapped_result['address'] = (
+            AddressMapper.map(resource['address']) if resource['address'] else None
+        )
         return mapped_result
 
 
