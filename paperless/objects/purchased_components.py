62e0d0b66f083c660019f9226028106b2789f43f
Alex Taxiera <alex.taxiera@paperlessparts.com>
2021-05-21 14:12:52 -0400

    update get/set purchased component property

--- paperless/objects/purchased_components.py
+++ paperless/objects/purchased_components.py
@@ -1,28 +1,43 @@
+from typing import List, Optional, Union
+
+import attr
+
 from paperless.json_encoders.purchased_components import PurchasedComponentEncoder
 from paperless.mixins import (
-  FromJSONMixin,
-  ListMixin,
-  ToJSONMixin,
-  ReadMixin,
-  UpdateMixin,
-  CreateMixin,
-  DeleteMixin,
-  PaginatedListMixin,
-) 
-from paperless.objects.utils import convert_iterable
-from typing import List, Optional, Union
-import attr
+    CreateMixin,
+    DeleteMixin,
+    FromJSONMixin,
+    ListMixin,
+    PaginatedListMixin,
+    ReadMixin,
+    ToJSONMixin,
+    UpdateMixin,
+)
 from paperless.objects.common import Money
+from paperless.objects.utils import convert_iterable
+
 
 @attr.s(frozen=False)
-class PurchasedComponentColumn(FromJSONMixin, ToJSONMixin, ReadMixin, UpdateMixin, CreateMixin, DeleteMixin, ListMixin):
+class PurchasedComponentColumn(
+    FromJSONMixin,
+    ToJSONMixin,
+    ReadMixin,
+    UpdateMixin,
+    CreateMixin,
+    DeleteMixin,
+    ListMixin,
+):
     id: int = attr.ib(validator=attr.validators.instance_of(int))
     name: str = attr.ib(validator=attr.validators.instance_of(str))
     code_name: str = attr.ib(validator=attr.validators.instance_of(str))
     value_type: str = attr.ib(validator=attr.validators.instance_of(str))
-    default_string_value: Optional[str] = attr.validators.optional(validator=attr.validators.instance_of(str))
+    default_string_value: Optional[str] = attr.validators.optional(
+        validator=attr.validators.instance_of(str)
+    )
     default_boolean_value: bool = attr.ib(validator=attr.validators.instance_of(bool))
-    default_numeric_valye: Optional[int] = attr.validators.optional(validator=attr.validators.instance_of(int))
+    default_numeric_valye: Optional[int] = attr.validators.optional(
+        validator=attr.validators.instance_of(int)
+    )
     position: int = attr.ib(validator=attr.validators.instance_of(int))
 
     @classmethod
@@ -51,15 +66,22 @@ class PurchasedComponentCustomProperty:
     key: str = attr.ib(validator=attr.validators.instance_of(str))
     value: Optional[Union[str, float, bool]] = attr.ib()
 
+
 @attr.s(frozen=False)
-class PurchasedComponent(FromJSONMixin, ToJSONMixin, ReadMixin, UpdateMixin, CreateMixin, DeleteMixin, PaginatedListMixin):
+class PurchasedComponent(
+    FromJSONMixin,
+    ToJSONMixin,
+    ReadMixin,
+    UpdateMixin,
+    CreateMixin,
+    DeleteMixin,
+    PaginatedListMixin,
+):
     _json_encoder = PurchasedComponentEncoder
 
     id: int = attr.ib(validator=attr.validators.instance_of(int))
     oem_part_number: str = attr.ib(validator=attr.validators.instance_of(str))
-    piece_price: str = attr.ib(
-        validator=attr.validators.instance_of(str)
-    )
+    piece_price: str = attr.ib(validator=attr.validators.instance_of(str))
     properties: List[PurchasedComponentCustomProperty] = attr.ib(
         converter=convert_iterable(PurchasedComponentCustomProperty)
     )
@@ -70,15 +92,19 @@ class PurchasedComponent(FromJSONMixin, ToJSONMixin, ReadMixin, UpdateMixin, Cre
         validator=attr.validators.optional(attr.validators.instance_of(str))
     )
 
-    def get_property(
-        self, code_name: str
+    def get_property(self, key: str) -> Optional[Union[str, float, bool]]:
+        """
+        Return the value of the property with the specified key or None
+        """
+        return next((pcp.value for pcp in self.properties if pcp.key == key), None)
+
+    def set_property(
+        self, key: str, value: Optional[Union[str, float, bool]]
     ) -> Optional[Union[str, float, bool]]:
         """
-        Return the value of the property with the specified code name or None
+        Update the value of the property with the specified code name
         """
-        return (
-            {pcp.code_name: pcp.value for pcp in self.properties}.get(code_name, None)
-        )
+        next(pcp for pcp in self.properties if pcp.key == key).value = value
 
     @classmethod
     def construct_delete_url(cls):
