1c4de71f24964f1d3624bd3ea528c80412fd5517
Amaan Ali <amaan.ali.int@paperlessparts.com>
2022-07-12 17:20:22 +0000

    Merged in feature/PP-10244-sdk-class-for-heartbeat-post (pull request #33)
    added heartbeat class
    
    * added heartbeat class
    
    * removed unnecessary things
    
    * added back the encoder - needed
    
    * fixed all
    
    * name changes
    
    * simple test
    
    * precommit hook
    
    
    Approved-by: Nirav Ilango
    Approved-by: Old Man Jenkins

---
+++ paperless/objects/integration_heartbeats.py
@@ -0,0 +1,28 @@
+from tokenize import Number
+from typing import Optional
+
+import attr
+
+from paperless.client import PaperlessClient
+from paperless.json_encoders.integration_heartbeats import IntegrationHeartbeatEncoder
+from paperless.mixins import CreateMixin, ToJSONMixin
+
+
+@attr.s(frozen=False)
+class IntegrationHeartbeat(ToJSONMixin, CreateMixin):
+    _json_encoder = IntegrationHeartbeatEncoder
+    interval = attr.ib(validator=attr.validators.instance_of(int))
+
+    @classmethod
+    def construct_post_url(cls, managed_integration_uuid):
+        return 'managed_integrations/{}/heartbeat'.format(managed_integration_uuid)
+
+    def create(self, managed_integration_uuid):
+        """
+        Persist new version of self to Paperless Parts and updates instance with any new data from the creation.
+        """
+        client = PaperlessClient.get_instance()
+        data = self.to_json()
+        client.create_resource(
+            self.construct_post_url(managed_integration_uuid), data=data
+        )
---
+++ tests/unit/test_integration_heartbeats.py
@@ -0,0 +1,25 @@
+import datetime
+import json
+import unittest
+from unittest.mock import MagicMock
+
+from paperless.client import PaperlessClient
+from paperless.objects.integration_heartbeats import IntegrationHeartbeat
+
+
+class TestIntegrationHeartbeat(unittest.TestCase):
+    def setUp(self):
+        self.client = PaperlessClient(
+            access_token="test_accesstoken", base_url="http://localhost/api"
+        )
+        with open(
+            'tests/unit/mock_data/integration_heartbeat.json'
+        ) as integration_heartbeat_data:
+            self.integration_heartbeat = json.load(integration_heartbeat_data)
+
+    def test_post_heartbeat(self):
+        self.client.create_resource = MagicMock(return_value=self.integration_heartbeat)
+        heartbeat = IntegrationHeartbeat(60).create(
+            managed_integration_uuid="712f4343-a29c-4263-be38-3c1694f53439"
+        )
+        self.assertEqual(heartbeat, None)
