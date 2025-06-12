25e6a06ba6a1cf38f69109d66db90c9755a23ed7
Vince Coccia <vince.coccia@paperlessparts.com>
2025-06-12 12:27:57 -0400

    add retrieval of paginated quote items
    Also addressed function naming, and abstracted a more general method for
    retrieving JSON encoded from a paginated API endpoint.

--- tests/unit/test_functions.py
+++ tests/unit/test_functions.py
@@ -3,10 +3,19 @@ import unittest
 from unittest.mock import MagicMock
 
 from paperless.client import PaperlessClient
-from paperless.functions.components import GetQuoteItemComponents
+from paperless.functions.components import get_quote_item_components, get_quote_items
 
 
-class TestGetQuoteItemComponent(unittest.TestCase):
+class TestGetPaginatedAPIObjects(unittest.TestCase):
+    class result_set:
+        def __init__(self, list_of_maps):
+            self.list = list_of_maps
+            self.called = 0
+
+        def next_item(self, _url, _params={}):
+            self.called += 1
+            return self.list.pop(0)
+
     def setUp(self):
         # instantiate client singleton
         self.client = PaperlessClient()
@@ -18,19 +27,13 @@ class TestGetQuoteItemComponent(unittest.TestCase):
 
     def test_invalid_starting_page(self):
         with self.assertRaises(ValueError):
-            GetQuoteItemComponents(1, 0)
+            get_quote_item_components(1, 0)
 
-    def test_get_everything(self):
+        with self.assertRaises(ValueError):
+            get_quote_items(1, 0)
+
+    def test_get_all_components(self):
         qi = self.quote_items[105565]
-
-        class result_set:
-            def __init__(self, list_of_maps):
-                self.list = list_of_maps
-                self.called = 0
-
-            def next_item(self, _url, _params={}):
-                self.called += 1
-                return self.list.pop(0)
 
         comps = [
             {
@@ -42,10 +45,70 @@ class TestGetQuoteItemComponent(unittest.TestCase):
             },
         ]
 
-        rs = result_set(comps)
+        rs = self.result_set(comps)
         self.client.get_resource_list = MagicMock(side_effect=rs.next_item)
 
-        results = GetQuoteItemComponents(1)
+        results = get_quote_item_components(1)
 
         self.assertEqual(4, len(results))
+        self.assertEqual(2, rs.called)
+
+    def test_get_all_quote_items(self):
+        api_response = [
+            {
+                'next': 'next_page_url',
+                'results': [
+                    {
+                        'id': 1,
+                        'component_ids': [2, 3, 4],
+                        'export_controlled': True,
+                        'metadata': {
+                            'stuff': 'more_stuff',
+                        },
+                        'position': 1,
+                        'private_notes': 's3kr3t',
+                        'public_notes': 'n0t s3kr3t',
+                        'type': 'the type',
+                        'workflow status': 'something',
+                    },
+                    {
+                        'id': 2,
+                        'component_ids': [5, 6, 7],
+                        'export_controlled': True,
+                        'metadata': {
+                            'stuff': 'more_stuff',
+                        },
+                        'position': 2,
+                        'private_notes': 's3kr3t',
+                        'public_notes': 'n0t s3kr3t',
+                        'type': 'the type',
+                        'workflow status': 'something',
+                    },
+                ],
+            },
+            {
+                'results': [
+                    {
+                        'id': 3,
+                        'component_ids': [2, 3, 4],
+                        'export_controlled': True,
+                        'metadata': {
+                            'stuff': 'more_stuff',
+                        },
+                        'position': 3,
+                        'private_notes': 's3kr3t',
+                        'public_notes': 'n0t s3kr3t',
+                        'type': 'the type',
+                        'workflow status': 'something',
+                    },
+                ],
+            },
+        ]
+
+        rs = self.result_set(api_response)
+        self.client.get_resource_list = MagicMock(side_effect=rs.next_item)
+
+        quote_items = get_quote_items(1)
+
+        self.assertEqual(3, len(quote_items))
         self.assertEqual(2, rs.called)
